75daacb53dd67518825eebcb647da78f17643940
azubuikestanley <azubuikestanley@gmail.com>
2019-12-12 17:38:39 +0100

    testing

--- server.js
+++ server.js
@@ -33,10 +33,12 @@ app.use('/api/items', items);
 //Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
     //Set static folder
-    app.use(express.static('client/build'));
+    // app.use(express.static(__dirname, 'client/build'));
+    app.use(express.static(path.join(__dirname, './client/build')));
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get('*', (req, res) => {
+        // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + './client/build/index.html'));
     });
 }
 
