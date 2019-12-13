acc25c0de376efa3c6c4e375a888ac03059d944b
azubuikestanley <azubuikestanley@gmail.com>
2019-12-13 10:00:20 +0100

    added jwt token and middleware to api

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Stanley:stan1234@cluster0-ikqwa.mongodb.net/test?retryWrites=true&w=majority'
-}
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 //Item Model
 const Item = require('../../models/Item');
@@ -11,12 +12,12 @@ router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items))
-});
+}); 
 
 //@route POST api/items
 //@desc Create A Post
-//@access Public
-router.post('/', (req, res) => {
+//@access Private
+router.post('/', auth, (req, res) => {
     const newItem = new Item({
         name: req.body.name
     });
@@ -26,8 +27,8 @@ router.post('/', (req, res) => {
 
 //@route DELETE api/items/:id
 //@description DELETE An Item
-//@access Public
-router.delete('/:id', (req, res) => {
+//@access Private
+router.delete('/:id', auth, (req, res) => {
     Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({success: true})))
     .catch(err => res.status(404).json({ success: false }));
